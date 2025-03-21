295c5725f9378794304a158aea679399da924237
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-21 18:30:27 -0300

    Exercício 12 concluído.

---
+++ Exercicio-12/Program.cs
@@ -0,0 +1,55 @@
+﻿using Utils;
+
+namespace Exercicio_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal initialSalary, increasedSalary, finalSalary;
+            bool onMenu;
+            do
+            {
+                Header();
+                initialSalary = Validators.DecimalVerify("Digite o salário do funcionário: ");
+                increasedSalary = SalaryIncrease(initialSalary);
+                finalSalary = SalaryDecrease(increasedSalary);
+
+
+                if (finalSalary == 0)
+                {
+                    ViewUtils.PaintWriteLine($"\nFuncionário acabou sem nada...\n", ConsoleColor.Red);
+                    onMenu = false;
+                    continue;
+                }
+                else
+                {
+                    ViewUtils.PaintWriteLine($"\nRESULTADO:", ConsoleColor.Green);
+                    ViewUtils.PaintWriteLine($"\nSalário inicial: R$ {initialSalary:F2}", ConsoleColor.Green);
+                    ViewUtils.PaintWriteLine($"\nSalário após Aumento: R$ {increasedSalary:F2}", ConsoleColor.Green);
+                    ViewUtils.PaintWriteLine($"\nSalário Final: R$ {finalSalary:F2}", ConsoleColor.Green);
+                }
+
+                onMenu = Validators.YesOrNo("\nDeseja fazer outro cálculo? (S/N) ");
+            } while (onMenu);
+            Console.Clear();
+            ViewUtils.PaintWriteLine("Adeus (T_T)/", ConsoleColor.Magenta);
+        }
+        static void Header()
+        {
+            DateTime date = DateTime.Now;
+            Console.Clear();
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==", ConsoleColor.Cyan);
+            ViewUtils.PaintWriteLine($" Calculadora - Salário | {date:dd/MM/yyyy HH:mm}");
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==", ConsoleColor.Cyan);
+        }
+        static decimal SalaryIncrease(decimal initialSalary)
+        {
+            return initialSalary * 1.15m;
+        }
+        static decimal SalaryDecrease(decimal increasedSalary)
+        {
+            return increasedSalary * 0.92m;
+        }
+    }
+}
