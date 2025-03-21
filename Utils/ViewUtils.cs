fe3bf92721e584125eaba1a89c2c5871519f6d6f
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-21 04:56:40 -0300

    feat: Criada `PressEnter` para melhor interação com o usuário.

--- Utils/ViewUtils.cs
+++ Utils/ViewUtils.cs
@@ -14,5 +14,23 @@
             Console.Write(message);
             Console.ResetColor();
         }
+        public static void PressEnter(string type)
+        {
+            switch (type)
+            {
+                case "TENTAR-NOVAMENTE":
+                    PaintWrite("Pressione [Enter] para tentar novamente.", ConsoleColor.Yellow);
+                    Console.ReadKey();
+                    break;
+                case "VOLTAR-MENU":
+                    PaintWrite("Pressione [Enter] para voltar ao menu principal.", ConsoleColor.Yellow);
+                    Console.ReadKey();
+                    break;
+                case "VER-RESULTADO":
+                    PaintWrite("Pressione [Enter] para ver o resultado.", ConsoleColor.Yellow);
+                    Console.ReadKey();
+                    break;
+            }
+        }
     }
 }
