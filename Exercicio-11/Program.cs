df8e5ef7d5bb25ddb8050c8bb4a2b8ae012ad9e5
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-21 18:19:34 -0300

    Exercício 11 concluído.

---
+++ Exercicio-11/Program.cs
@@ -0,0 +1,47 @@
+﻿using Utils;
+
+namespace Exercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age, daysLived;
+            bool onMenu;
+            do
+            {
+                Header();
+                ViewUtils.PaintWrite("Digite seu nome: ");
+                name = Validators.StringVerify("Tem certeza que esse é seu nome?", 2);
+                age = Validators.IntVerify("Qual a sua idade? ");
+                daysLived = Result(age);
+
+                if (daysLived <= 2555)
+                {
+                    ViewUtils.PaintWriteLine($"\n Então, {name}.. Você não deveria estar aqui...\n", ConsoleColor.Red);
+                    onMenu = false;
+                    continue;
+                }
+                else
+                    ViewUtils.PaintWriteLine($"\n{name}, você já vivode {daysLived}!!", ConsoleColor.Green);
+
+                onMenu = Validators.YesOrNo("\nDeseja fazer outro cálculo? (S/N) ");
+            } while (onMenu);
+            Console.Clear();
+            ViewUtils.PaintWriteLine("Adeus (T_T)/", ConsoleColor.Magenta);
+        }
+        static void Header()
+        {
+            DateTime date = DateTime.Now;
+            Console.Clear();
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==", ConsoleColor.Cyan);
+            ViewUtils.PaintWriteLine($"  Calculadora - Dias Vividos || {date:dd/MM/yyyy HH:mm}");
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==", ConsoleColor.Cyan);
+        }
+        static int Result(int age)
+        {
+            return age * 365;
+        }
+    }
+}
