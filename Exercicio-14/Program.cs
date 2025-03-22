688b2a21c8b959757ced9480ced9e6fa23a00ac1
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-21 21:51:49 -0300

    Exercício 14 concluído.

---
+++ Exercicio-14/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Utils;
+
+namespace Exercicio_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber, secondNumber, thirdNumber;
+            bool onMenu;
+            do
+            {
+                Header();
+                firstNumber = Validators.DoubleVerify("Digite o valor de A: ");
+                secondNumber = Validators.DoubleVerify("Digite o valor de B: ");
+                thirdNumber = Validators.DoubleVerify("Digite o valor de C: ");
+
+                DescendingOrganizer(firstNumber, secondNumber, thirdNumber);
+
+                onMenu = Validators.YesOrNo("\nDeseja fazer outro cálculo? (S/N) ");
+            } while (onMenu);
+            Console.Clear();
+            ViewUtils.PaintWriteLine("Adeus (T_T)/", ConsoleColor.Magenta);
+        }
+        static void Header()
+        {
+            DateTime date = DateTime.Now;
+            Console.Clear();
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==", ConsoleColor.Cyan);
+            ViewUtils.PaintWriteLine($"  Organizador de Valores Inteiros | {date:dd/MM/yyyy HH:mm}");
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==", ConsoleColor.Cyan);
+        }
+        static void DescendingOrganizer(double firstNumber, double secondNumber, double thirdNumber)
+        {
+            double[] numbers = { firstNumber, secondNumber, thirdNumber };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double highestNumber = 0;
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[j] > highestNumber)
+                    {
+                        highestNumber = numbers[j];
+                    }
+                }
+                ViewUtils.PaintWriteLine(highestNumber.ToString());
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[j] == highestNumber)
+                    {
+                        numbers[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
