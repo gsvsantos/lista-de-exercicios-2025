fca47e9447c74778497c71f1c259ec215fbf6039
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-22 00:38:20 -0300

    Exercício 22 concluído.

---
+++ Exercicio-22/Program.cs
@@ -0,0 +1,44 @@
+﻿using Utils;
+
+namespace Exercicio_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            bool onMenu;
+            do
+            {
+                Header();
+                number = Validators.IntVerify("Digite um número: ");
+                FibonacciCalc(number);
+                onMenu = Validators.YesOrNo("\n\nDeseja ver novamente? (S/N) ");
+            } while (onMenu);
+            Console.Clear();
+            ViewUtils.PaintWriteLine("Adeus (T_T)/", ConsoleColor.Magenta);
+        }
+        static void Header()
+        {
+            DateTime date = DateTime.Now;
+            Console.Clear();
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==", ConsoleColor.Cyan);
+            ViewUtils.PaintWriteLine($"  Calculadora - Fibonacci | {date:dd/MM/yyyy HH:mm}");
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==\n", ConsoleColor.Cyan);
+        }
+        static void FibonacciCalc(int number)
+        {
+            int pastValue = 0;
+            int actualValue = 1;
+
+            ViewUtils.PaintWrite($"|");
+            while (pastValue <= number)
+            {
+                ViewUtils.PaintWrite($" {pastValue} |");
+                int nextValue = pastValue + actualValue;
+                pastValue = actualValue;
+                actualValue = nextValue;
+            }
+        }
+    }
+}
