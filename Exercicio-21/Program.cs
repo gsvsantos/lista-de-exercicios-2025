10278842c2ab2aa6f68ef172a72ad8f24e1ee54e
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-21 23:58:35 -0300

    Exercício 21 concluído.

---
+++ Exercicio-21/Program.cs
@@ -0,0 +1,41 @@
+﻿using Utils;
+
+namespace Exercicio_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            bool onMenu;
+            do
+            {
+                Header();
+                number = Validators.IntVerify("Digite um número: ");
+                FactorialCalc(number);
+                onMenu = Validators.YesOrNo("\nDeseja ver novamente? (S/N) ");
+            } while (onMenu);
+            Console.Clear();
+            ViewUtils.PaintWriteLine("Adeus (T_T)/", ConsoleColor.Magenta);
+        }
+        static void Header()
+        {
+            DateTime date = DateTime.Now;
+            Console.Clear();
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==", ConsoleColor.Cyan);
+            ViewUtils.PaintWriteLine($"  Calculadora - Fatorial || {date:dd/MM/yyyy HH:mm}");
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==\n", ConsoleColor.Cyan);
+        }
+        static void FactorialCalc(int number)
+        {
+            int factorial = 1;
+            Header();
+            for (int i = number; i > 1; i--)
+            {
+                factorial *= i;
+            }
+
+            ViewUtils.PaintWriteLine($"O resultado de {number}! é: {factorial}");
+        }
+    }
+}
