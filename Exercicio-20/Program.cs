a4fead2160a3b2ff2b0e8f8cbce7d74bb73a027a
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-21 23:47:53 -0300

    Exercício 20 concluído.

---
+++ Exercicio-20/Program.cs
@@ -0,0 +1,39 @@
+﻿using Utils;
+
+namespace Exercicio_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number;
+            bool onMenu;
+            do
+            {
+                Header();
+                number = Validators.DecimalVerify("Digite um número: ");
+                MultiplicationTable(number);
+                onMenu = Validators.YesOrNo("\nDeseja ver novamente? (S/N) ");
+            } while (onMenu);
+            Console.Clear();
+            ViewUtils.PaintWriteLine("Adeus (T_T)/", ConsoleColor.Magenta);
+        }
+        static void Header()
+        {
+            DateTime date = DateTime.Now;
+            Console.Clear();
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==", ConsoleColor.Cyan);
+            ViewUtils.PaintWriteLine($" Calculadora - Tabuada até 10 || {date:dd/MM/yyyy HH:mm}");
+            ViewUtils.PaintWriteLine("==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==\n", ConsoleColor.Cyan);
+        }
+       static void MultiplicationTable(decimal number)
+        {
+            Header();
+            for (int i = 1; i <= 10; i++)
+            {
+                decimal result = number * i;
+                ViewUtils.PaintWriteLine($"{number} x {i} = {result}", ConsoleColor.White);
+            }
+        }
+    }
+}
